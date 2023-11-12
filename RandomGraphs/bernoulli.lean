832a0faf05c7ce7449c7043a5007d2f482bf70dd
Vasily Ilin <vasilin97@gmail.com>
2023-11-11 22:25:17 -0800

    Prove E(B)=p for bernoulli B.

--- RandomGraphs/bernoulli.lean
+++ RandomGraphs/bernoulli.lean
@@ -5,41 +5,29 @@ import Mathlib.Probability.Moments
 
 open MeasureTheory ProbabilityTheory NNReal
 
--- 0 ≤ p ≤ 1
-variable (p : NNReal) (hp : p ≤ 1)
-
--- like `hp` but with type `ENNReal`
-lemma hp' : (p : ENNReal) ≤ 1 := by
-  simp [hp]
-
--- two random variables X and Y
-variable (X Y : Ω → ℝ)
--- We don't actually care what the probability space is
-variable [MeasurableSpace Ω] (μ : Measure Ω) [IsProbabilityMeasure μ]
--- X and Y are Bernoulli. I'm not sure what is the best definition.
-variable
-  (hX : Measure.map (fun ω ↦ if (X ω = 1) then true else false) μ = (PMF.bernoulli p (hp' p hp)).toMeasure)
-  (h0Y : μ {w | Y w = 0} = 1-p) (h1Y : μ {w | Y w = 1} = p)
--- X and Y are independent
-variable (hIndep : IndepFun X Y μ)
-
 structure Bernoulli
-  (B : Ω → ℕ) (p : NNReal)
-  [MeasurableSpace Ω] (μ : Measure Ω) [IsProbabilityMeasure μ]
+  [MeasurableSpace Ω]
+  (B : SimpleFunc Ω ℝ) (p : NNReal)
+  (μ : Measure Ω) [IsProbabilityMeasure μ]
   : Prop :=
   (le_one : p ≤ 1)
+  (range : SimpleFunc.range B = {0, 1})
   (prob_0 : μ {w | B w = 0} = 1-p)
   (prob_1 : μ {w | B w = 1} = p)
 
-#check Bernoulli
+variable [MeasurableSpace Ω]
 
--- ∫ |X| ∂μ is finite
-@[simp]
-lemma integrable [Fintype Ω] : Integrable X μ := by
-  sorry
+lemma measurable (B : SimpleFunc Ω ℝ) (μ : Measure Ω) : AEStronglyMeasurable B μ := SimpleFunc.aestronglyMeasurable B
+lemma integrable (B : SimpleFunc Ω ℝ) (μ : Measure Ω) [IsProbabilityMeasure μ] : Integrable B μ := SimpleFunc.integrable_of_isFiniteMeasure B
 
 -- ∫ X ∂μ = p
 @[simp]
-lemma bernoulli_expectation : ∫ ω, X ω ∂μ = p := by
-  -- split integral into two parts, {w | X w = 0} and {w | X w = 1}
+lemma bernoulli_expectation_ (B : SimpleFunc Ω ℝ) (p : NNReal) (μ : Measure Ω) [IsProbabilityMeasure μ] (h : Bernoulli B p μ) : SimpleFunc.integral μ B = p := by
+  simp only [SimpleFunc.integral_eq, h.range, Finset.mem_singleton, zero_ne_one, smul_eq_mul,
+    not_false_eq_true, Finset.sum_insert, mul_zero, Finset.sum_singleton, mul_one, zero_add]
+  have : μ (B⁻¹' {1}) = p := h.prob_1
+  simp only [ENNReal.coe_toReal, this]
+
+lemma bernoulli_expectation (B : SimpleFunc Ω ℝ) (p : NNReal) (μ : Measure Ω) [IsProbabilityMeasure μ] (h : Bernoulli B p μ) : ∫ ω, B ω ∂μ = p := by
+  -- simp [bernoulli_expectation_]
   sorry
