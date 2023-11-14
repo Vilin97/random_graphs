836dbfd1816e456a968f2b079c3ddbc043847cd3
Vasily Ilin <vasilin97@gmail.com>
2023-11-13 18:58:10 -0800

    Clean up bernoulli definition and expectation.

--- RandomGraphs/bernoulli.lean
+++ RandomGraphs/bernoulli.lean
@@ -1,5 +1,3 @@
-import Mathlib.Probability.ProbabilityMassFunction.Constructions
-import Mathlib.Data.Real.Basic
 import Mathlib.MeasureTheory.Integral.Bochner
 import Mathlib.Probability.Moments
 
@@ -12,24 +10,20 @@ structure Bernoulli
   : Prop :=
   (le_one : p ≤ 1)
   (range : SimpleFunc.range B = {0, 1})
-  (prob_0 : μ {w | B w = 0} = 1-p)
-  (prob_1 : μ {w | B w = 1} = p)
+  (prob_0 : μ (B⁻¹' {0}) = 1-p)
+  (prob_1 : μ (B⁻¹' {1}) = p)
 
 variable [MeasurableSpace Ω]
 
+@[simp]
 lemma measurable (B : SimpleFunc Ω ℝ) (μ : Measure Ω) : AEStronglyMeasurable B μ := SimpleFunc.aestronglyMeasurable B
+@[simp]
 lemma integrable (B : SimpleFunc Ω ℝ) (μ : Measure Ω) [IsProbabilityMeasure μ] : Integrable B μ := SimpleFunc.integrable_of_isFiniteMeasure B
 
 -- ∫ X ∂μ = p
 @[simp]
-lemma bernoulli_expectation_ (B : SimpleFunc Ω ℝ) (p : NNReal) (μ : Measure Ω) [IsProbabilityMeasure μ] (h : Bernoulli B p μ) : SimpleFunc.integral μ B = p := by
+lemma bernoulli_expectation {B : SimpleFunc Ω ℝ} {p : NNReal} {μ : Measure Ω} [IsProbabilityMeasure μ] (h : Bernoulli B p μ) : ∫ ω, B ω ∂μ = p := by
+  rw [←SimpleFunc.integral_eq_integral B (integrable B μ)]
   simp only [SimpleFunc.integral_eq, h.range, Finset.mem_singleton, zero_ne_one, smul_eq_mul,
-    not_false_eq_true, Finset.sum_insert, mul_zero, Finset.sum_singleton, mul_one, zero_add]
-  have : μ (B⁻¹' {1}) = p := h.prob_1
-  simp only [ENNReal.coe_toReal, this]
-
-lemma bernoulli_expectation (B : SimpleFunc Ω ℝ) (p : NNReal) (μ : Measure Ω) [IsProbabilityMeasure μ] (h : Bernoulli B p μ) : ∫ ω, B ω ∂μ = p := by
-  -- rw [integral_eq B]
-  rw [←SimpleFunc.integral_eq_integral B]
-  apply bernoulli_expectation_ B p μ h
-  apply integrable B μ
+    not_false_eq_true, Finset.sum_insert, mul_zero, Finset.sum_singleton, h.prob_1,
+    ENNReal.coe_toReal, mul_one, zero_add]
