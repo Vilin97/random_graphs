c82e7d5b4af1518df2b318cdbdf719b11e898f9d
dreampuzzleee <xiaoxiaodai@yahoo.com>
2024-02-21 17:07:50 -0800

    Yujia attempt

--- RandomGraphs/prob_of_connected.lean
+++ RandomGraphs/prob_of_connected.lean
@@ -1,4 +1,5 @@
 import RandomGraphs.erdos_renyi
+import RandomGraphs.isolated_vertex
 import Mathlib.Analysis.SpecialFunctions.Log.Basic
 import Mathlib.Data.Complex.Exponential
 
@@ -10,6 +11,16 @@ open MeasureTheory ProbabilityTheory BigOperators
 variable [Fintype V] [DecidableEq V]
 variable [MeasurableSpace Ω] (μ : Measure Ω) [IsProbabilityMeasure μ]
 variable (G : Ω → SimpleGraph V) [∀ ω e, Decidable (Edge (G ω) e)]
+
+
+-- the expected value of isolated vertex indicator is (1-p)^(n-1)
+theorem isolated_vertex_expectation (h : ErdosRenyi G p μ) (hn : Fintype.card V = n) (vi : V) : ∫ ω, (IsolatedVertexInd G vi) ω ∂μ = (1 - p)^(n - 1) := by
+  simp only [bernoulli_expectation (h.bernoulli_edges)]
+  sorry
+
+-- the expected vaule of number of isolated vertices in Graph G_{n,p} is n(1-p)^(n-1)
+theorem isolated_vertices_ingraph_expectation  (h : ErdosRenyi G p μ) (hn : Fintype.card V = n) (vi : V): ∫ ω, (IsolatedVertexInd G vi) ω ∂μ = n*(1 - p)^(n - 1) := by
+sorry
 
 -- probability of connected graph is exp(-exp(-c))
 theorem probability_of_connected (hp : p = ((Real.log n) + c)/n) (hG : ErdosRenyi G p μ) : μ { ω | (G ω).is_connected } = Real.exp (-Real.exp (-c)) := by
