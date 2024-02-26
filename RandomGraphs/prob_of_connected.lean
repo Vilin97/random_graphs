6c295ce0e87e8585ec76c45156f7cca0085a675a
dreampuzzleee <xiaoxiaodai@yahoo.com>
2024-02-26 11:15:46 -0800

    Yujia try

--- RandomGraphs/prob_of_connected.lean
+++ RandomGraphs/prob_of_connected.lean
@@ -13,12 +13,12 @@ variable [MeasurableSpace Ω] (μ : Measure Ω) [IsProbabilityMeasure μ]
 variable (G : Ω → SimpleGraph V) [∀ ω e, Decidable (Edge (G ω) e)]
 
 -- the expected value of isolated vertex indicator is (1-p)^(n-1)
-theorem isolated_vertex_expectation (h : ErdosRenyi G p μ) (hn : Fintype.card V = n) (vi : V) : ∫ ω, (IsolatedVertexInd G vi) ω ∂μ = (1 - p)^(n - 1) := by
+theorem isolated_vertex_expectation (h : ErdosRenyi G p μ) (hn : Fintype.card V = n) (vi : V) : ∫ ω, (isolated_vertex_3 G vi) ω ∂μ = (1 - p)^(n - 1) := by
   simp only [bernoulli_expectation (h.bernoulli_edges)]
   sorry
 
 -- the expected vaule of number of isolated vertices in Graph G_{n,p} is n(1-p)^(n-1)
-theorem isolated_vertices_ingraph_expectation  (h : ErdosRenyi G p μ) (hn : Fintype.card V = n) (vi : V): ∫ ω, (IsolatedVertexInd G vi) ω ∂μ = n*(1 - p)^(n - 1) := by
+theorem isolated_vertices_ingraph_expectation  (h : ErdosRenyi G p μ) (hn : Fintype.card V = n) (vi : V): ∫ ω, (isolated_vertex_3 G vi) ω ∂μ = n*(1 - p)^(n - 1) := by
 sorry
 
 -- probability of connected graph is exp(-exp(-c))
