1a403704fb98abab33c848a08ca8c570585f24b0
the-Fish2 <nishakmk2@gmail.com>
2024-03-05 21:29:26 -0800

    Compiled theorem statements

--- RandomGraphs/prob_of_connected.lean
+++ RandomGraphs/prob_of_connected.lean
@@ -18,8 +18,8 @@ variable (G : Ω → SimpleGraph V) [∀ ω e, Decidable (Edge (G ω) e)]
 
 -- the expected value of isolated vertex indicator is (1-p)^(n-1)
 -- theorem isolated_vertex_expectation (h : ErdosRenyi G p μ) (hn : Fintype.card V = n) (vi : V) : ∫ ω, (isolated_vertex_3 G vi) ω ∂μ = (1 - p)^(n - 1) := by
-  --simp only [bernoulli_expectation (h.bernoulli_edges)]
-  --sorry
+--   simp only [bernoulli_expectation (h.bernoulli_edges)]
+--   sorry
 
 -- the expected vaule of number of isolated vertices in Graph G_{n,p} is n(1-p)^(n-1)
 -- theorem isolated_vertices_ingraph_expectation  (h : ErdosRenyi G p μ) (hn : Fintype.card V = n) (vi : V): ∫ ω, (isolated_vertex_3 G vi) ω ∂μ = n*(1 - p)^(n - 1) := by
@@ -29,7 +29,17 @@ variable (G : Ω → SimpleGraph V) [∀ ω e, Decidable (Edge (G ω) e)]
 -- theorem probability_of_connected (hp : p = ((Real.log n) + c)/n) (hG : ErdosRenyi G (Real.toNNReal p) μ) : Filter.tendsto (λ (n: ℕ → ℝ) μ { ω | (G ω).Connected} filter.at_top (nhds (Real.toNNReal (Real.exp (-Real.exp (-c)))))):= by
 -- sorry
 
--- theorem taylor_series_log_expansion (x : ℝ) (hx: |x| < 1) : Filter.TendsTo (λ (n : ℕ) ∑ i in finset.range n) (-(x^i)/i) (log (1 - x)) := by
+
+-- Unsure how to remove the Σ i in finset.range n segment
+
+-- theorem taylor_series_log_expansion (x : ℝ) (hx: |x| < 1) : Filter.TendsTo (λ (n : ℕ) ∑ i in finset.range n, (-(x^i)/i)) atTop (log (1 - x)) := by
+--   sorry
+
+-- theorem taylor_series_bounding (c : ℝ) (n: ℕ) : Filter.TendsTo (fun n => Σ i in finset.range n, (Real.toNNReal ((Real.log n) + c)/ n)^n) atTop (n * p²) := by
+--   sorry
+
+
+-- theorem main_theorem (c: ℝ) (Gn : ℕ → Ω → SimpleGraph V) (h: ∀ n ∈ ℕ, ErdosRenyi (Gn n) (Real.toNNReal ((Real.log n) + c) / n) μ): Filter.Tendsto (fun n => ENNReal.toReal (μ {ω | (Gn n ω).Connected})) Filter.atTop (nhds (Real.exp (-Real.exp (-c)))) := by
 --   sorry
 
 
